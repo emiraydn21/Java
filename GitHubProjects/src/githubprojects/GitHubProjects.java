b286ee3686d812a5119e117a3c5b886095786261
Emir <emir.aydn21@gmail.com>
2022-12-25 21:26:29 +0300

    İlk Commit
    Githuba Yükleyeceğimiz İlk Proje

---
+++ GitHubProjects/src/githubprojects/GitHubProjects.java
@@ -0,0 +1,15 @@
+
+package githubprojects;
+
+/**
+ *
+ * @author Emir AYDIN
+ */
+public class GitHubProjects {
+
+  
+    public static void main(String[] args) {
+        System.out.println("GitHuba Proje Yükleyeceğiz.");
+    }
+    
+}
